2b167c86aa2f32bd577a4f11b2faa62788978e5a
davidpacco <david.j.pacco@gmail.com>
2024-08-08 10:44:53 -0500

    Add error handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const mongoose = require('mongoose')
 const Person = require('./models/person')
+const { GraphQLError } = require("graphql")
 
 require('dotenv').config()
 
@@ -72,12 +73,34 @@ const resolvers = {
   Mutation: {
     addPerson: async (root, args) => {
       const person = new Person({ ...args })
-      return person.save()
+
+      try {
+        return person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving person failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
     },
     editNumber: async (root, args) => {
       const person = await Person.findOne({ name: args.name })
       person.phone = args.phone
-      return person.save()
+
+      try {
+        return person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving number failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
     }
   }
 }
